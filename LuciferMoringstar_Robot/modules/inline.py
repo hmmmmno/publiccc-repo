8f04227c284da5d34566b990258a1f0d6af99d64
hmmmmno <104258190+hmmmmno@users.noreply.github.com>
2022-05-26 14:46:21 +0530

    Update inline.py

--- LuciferMoringstar_Robot/modules/inline.py
+++ LuciferMoringstar_Robot/modules/inline.py
@@ -78,8 +78,8 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Support Group', url='t.me/Mo_Tech_Group'),
-        InlineKeyboardButton('More Botz', url='t.me/MT_Botz')
+        InlineKeyboardButton('Support Group', url='t.me/Dark_World7_4'),
+        InlineKeyboardButton('More Botz', url='t.me/rangermoviess')
         ],[
         InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
         ]]
